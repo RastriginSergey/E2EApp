f1b43811d9b27ae30fe7885ef2e2bdec7cc90ddc
Sergei Rastrigin <sergei.rastrigin@vonage.com>
2020-01-31 17:21:48 +0000

    move pods commands to pre-build script

--- E2EApp/appcenter-post-build.sh
+++ E2EApp/appcenter-post-build.sh
@@ -1,16 +1,5 @@
 #!/bin/bash
 
-podVersion=`pod --version`
-
-if [ ! $podVersion ]; then
-    echo ERROR: Failed to install Cocoapods
-    exit 1
-fi
-
-echo "Cocoapods version is:" $podVersion
-
-pod install
-
 xcrun xcodebuild build-for-testing \
   -configuration Debug \
   -project E2EApp.xcodeproj \
