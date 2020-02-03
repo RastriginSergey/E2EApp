d96572e67b36145e436a7937ef98489fdb0c1d0b
Sergei Rastrigin <sergei.rastrigin@vonage.com>
2020-02-03 11:57:38 +0000

    added post-build command

--- E2EApp/appcenter-post-build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-xcrun xcodebuild build-for-testing \
-  -configuration Debug \
-  -project E2EApp.xcodeproj \
-  -sdk iphoneos \
-  -scheme E2EApp \
-  -derivedDataPath DerivedData
---
+++ appcenter-post-build.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+appcenter test run xcuitest --app "My_Test/AppForE2E" \
+--devices "My_Test/iphone-7-plus-13-dot-3-1" \
+--test-series "master" \
+--locale "en_US" \
+--build-dir "DerivedData/E2EApp/Build/Products/Debug-iphonesimulator"
