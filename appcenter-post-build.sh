d56b9a333b57d16a698029237fa969436f6eb095
Sergei Rastrigin <sergei.rastrigin@vonage.com>
2020-02-03 14:42:52 +0000

    remove post build script

--- appcenter-post-build.sh
+++
@@ -1,7 +0,0 @@
-echo "Executing post build script"
-
-appcenter test run xcuitest --app "My_Test/AppForE2E" \
---devices "My_Test/iphone-7-plus-13-dot-3-1" \
---test-series "master" \
---locale "en_US" \
---build-dir "DerivedData/E2EApp/Build/Products/Debug-iphonesimulator"
