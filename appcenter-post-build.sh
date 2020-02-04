fac402bd5b195b4700fb9be1f9e2c5e9ac186ef3
Sergei Rastrigin <sergei.rastrigin@vonage.com>
2020-02-04 17:26:23 +0000

    change paths of a build directory

--- appcenter-post-build.sh
+++ appcenter-post-build.sh
@@ -4,10 +4,11 @@ echo "post build script was executed"
 
 env
 
+
 appcenter test run xcuitest \
 --app "ashley.arthur-vonage.com/IOS-SDK_TEST" \
 --devices "ashley.arthur-vonage.com/ios11" \
 --test-series "master" \
 --locale "en_US" \
 --token "1b2050ed79bfa481249056ef0970e19938771312" \
---build-dir "${APPCENTER_OUTPUT_DIRECTORY}/DerivedData/Build/Products/Debug-iphoneos"
+--build-dir "DerivedData/Build/Products/Debug-iphoneos"
