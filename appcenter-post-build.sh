7cf1d1eef8adaa311f87143cd2af96e9436dcd78
Sergei Rastrigin <sergei.rastrigin@vonage.com>
2020-02-04 16:06:12 +0000

    hardcoded a token for tests run

--- appcenter-post-build.sh
+++ appcenter-post-build.sh
@@ -7,4 +7,5 @@ appcenter test run xcuitest \
 --devices "ashley.arthur-vonage.com/ios11" \
 --test-series "master" \
 --locale "en_US" \
+--token "1b2050ed79bfa481249056ef0970e19938771312" \
 --build-dir DerivedData/Build/Products/Debug-iphoneos
