abb3392a77ba79623f6da44909833820b8bac234
Sergei Rastrigin <sergei.rastrigin@vonage.com>
2020-02-04 15:52:55 +0000

    run xcuitests from post build script

--- appcenter-post-build.sh
+++ appcenter-post-build.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/env bash
 
 echo "post build script was executed"
+
+appcenter test run xcuitest \
+--app "ashley.arthur-vonage.com/IOS-SDK_TEST" \
+--devices "ashley.arthur-vonage.com/ios11" \
+--test-series "master" \
+--locale "en_US" \
+--build-dir DerivedData/Build/Products/Debug-iphoneos
