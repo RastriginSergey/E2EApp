dd8801d74c2ef0ec9ae42841d8c84c3eaa02503f
Sergei Rastrigin <sergei.rastrigin@vonage.com>
2020-02-04 17:59:05 +0000

    lookup for folders

--- appcenter-post-build.sh
+++ appcenter-post-build.sh
@@ -4,10 +4,20 @@ echo "post build script was executed"
 
 pwd
 
+ls -lah
+
+dir_resolve()
+{
+cd "$1" 2>/dev/null || return $?  # cd to desired directory; if fail, quell any error messages but return exit status
+echo "`pwd -P`" # output full, link-resolved path
+}
+
+abs_path="`dir_resolve \"/Users/runner/Library/Developer/Xcode/DerivedData/E2EApp-*/DerivedData/Build/Products/Debug-iphoneos\"`"
+
 appcenter test run xcuitest \
 --app "ashley.arthur-vonage.com/IOS-SDK_TEST" \
 --devices "ashley.arthur-vonage.com/ios11" \
 --test-series "master" \
 --locale "en_US" \
 --token "1b2050ed79bfa481249056ef0970e19938771312" \
---build-dir "DerivedData/Build/Products/Debug-iphoneos"
+--build-dir "$abs_path"
