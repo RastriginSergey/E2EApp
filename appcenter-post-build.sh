976324b223f7990fb327f3abffc03886c4bc43aa
Sergei Rastrigin <sergei.rastrigin@vonage.com>
2020-02-05 12:18:15 +0000

    sdf

--- appcenter-post-build.sh
+++ appcenter-post-build.sh
@@ -4,12 +4,16 @@ echo "post build script was executed"
 
 cd "/Users/runner/Library/Developer/Xcode/DerivedData/E2EApp-*/Build/"
 
-BUILD_PATH=pwd
+BUILD_PATH=$(pwd)
 
-appcenter test run xcuitest \
---app "ashley.arthur-vonage.com/IOS-SDK_TEST" \
---devices "ashley.arthur-vonage.com/ios11" \
---test-series "master" \
---locale "en_US" \
---token "1b2050ed79bfa481249056ef0970e19938771312" \
---build-dir $BUILD_PATH
+echo $BUILD_PATH
+
+ls -R $BUILD_PATH
+
+#appcenter test run xcuitest \
+#--app "ashley.arthur-vonage.com/IOS-SDK_TEST" \
+#--devices "ashley.arthur-vonage.com/ios11" \
+#--test-series "master" \
+#--locale "en_US" \
+#--token "1b2050ed79bfa481249056ef0970e19938771312" \
+#--build-dir $BUILD_PATH
