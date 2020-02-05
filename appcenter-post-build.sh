965c7e4733ae2d3ce56424d2061589c38e3f7ee3
Sergei Rastrigin <sergei.rastrigin@vonage.com>
2020-02-05 15:19:10 +0000

    add build script

--- appcenter-post-build.sh
+++ appcenter-post-build.sh
@@ -1,9 +1,23 @@
 ##!/usr/bin/env bash
-#
-#appcenter test run xcuitest \
-#--app "ashley.arthur-vonage.com/IOS-SDK_TEST" \
-#--devices "ashley.arthur-vonage.com/ios11" \
-#--test-series "master" \
-#--locale "en_US" \
-#--token "1b2050ed79bfa481249056ef0970e19938771312" \
-#--build-dir "DerivedData/Build/Products/Debug-iphoneos"
+
+echo "================================="
+echo "ENVIRONEMNT VARIABLES"
+env
+echo "================================="
+
+rm -rf DerivedData
+xcrun xcodebuild build-for-testing \
+  -configuration Debug \
+  -workspace $APPCENTER_XCODE_PROJECT \
+  -sdk iphoneos \
+  -scheme $APPCENTER_XCODE_SCHEME \
+  -derivedDataPath DerivedData
+
+
+appcenter test run xcuitest \
+--app "ashley.arthur-vonage.com/IOS-SDK_TEST" \
+--devices "ashley.arthur-vonage.com/ios11" \
+--test-series "master" \
+--locale "en_US" \
+--token "1b2050ed79bfa481249056ef0970e19938771312" \
+--build-dir "DerivedData/Build/Products/Debug-iphoneos"
