d1452af332444976e50f5d82ba00b6979797c768
Sergei Rastrigin <sergei.rastrigin@vonage.com>
2020-02-05 12:17:09 +0000

    sdf

--- appcenter-post-build.sh
+++ appcenter-post-build.sh
@@ -1,22 +1,10 @@
 #!/usr/bin/env bash
 
 echo "post build script was executed"
-#
-#pwd
-#
-#ls -lah
-#
-#dir_resolve()
-#{
-#cd "$1" 2>/dev/null || return $?  # cd to desired directory; if fail, quell any error messages but return exit status
-#echo "`pwd -P`" # output full, link-resolved path
-#}
-#
-#abs_path="`dir_resolve \"/Users/runner/Library/Developer/Xcode/DerivedData/E2EApp-*/DerivedData/Build/Products/Debug-iphoneos\"`"
 
-#env
+cd "/Users/runner/Library/Developer/Xcode/DerivedData/E2EApp-*/Build/"
 
-ls -R $HOME
+BUILD_PATH=pwd
 
 appcenter test run xcuitest \
 --app "ashley.arthur-vonage.com/IOS-SDK_TEST" \
@@ -24,8 +12,4 @@ appcenter test run xcuitest \
 --test-series "master" \
 --locale "en_US" \
 --token "1b2050ed79bfa481249056ef0970e19938771312" \
---build-dir "${APPCENTER_XCODE_PROJECT}/DerivedData/Build/Products/Debug-iphoneos"
-
-echo $APPCENTER_OUTPUT_DIRECTORY
-
-ls -lah $APPCENTER_OUTPUT_DIRECTORY
+--build-dir $BUILD_PATH
