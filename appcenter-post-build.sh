96fe474d71d1f30b2037a677d4cb016ec0e47199
Sergei Rastrigin <sergei.rastrigin@vonage.com>
2020-02-05 11:30:30 +0000

    use $APPCENTER_OUTPUT_DIRECTORY as a build dir path

--- appcenter-post-build.sh
+++ appcenter-post-build.sh
@@ -14,14 +14,10 @@ echo "post build script was executed"
 #
 #abs_path="`dir_resolve \"/Users/runner/Library/Developer/Xcode/DerivedData/E2EApp-*/DerivedData/Build/Products/Debug-iphoneos\"`"
 
-#appcenter test run xcuitest \
-#--app "ashley.arthur-vonage.com/IOS-SDK_TEST" \
-#--devices "ashley.arthur-vonage.com/ios11" \
-#--test-series "master" \
-#--locale "en_US" \
-#--token "1b2050ed79bfa481249056ef0970e19938771312" \
-#--build-dir "$abs_path"
-
-echo $APPCENTER_OUTPUT_DIRECTORY
-
-ls -lah $APPCENTER_OUTPUT_DIRECTORY
+appcenter test run xcuitest \
+--app "ashley.arthur-vonage.com/IOS-SDK_TEST" \
+--devices "ashley.arthur-vonage.com/ios11" \
+--test-series "master" \
+--locale "en_US" \
+--token "1b2050ed79bfa481249056ef0970e19938771312" \
+--build-dir $APPCENTER_OUTPUT_DIRECTORY
