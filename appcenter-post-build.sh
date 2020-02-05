38f098f8cd0afce5e990893836ca4b47b25bc14c
Sergei Rastrigin <sergei.rastrigin@vonage.com>
2020-02-05 11:05:30 +0000

    lookup in APPCENTER_OUTPUT_DIRECTORY environment variable

--- appcenter-post-build.sh
+++ appcenter-post-build.sh
@@ -1,23 +1,27 @@
 #!/usr/bin/env bash
 
 echo "post build script was executed"
+#
+#pwd
+#
+#ls -lah
+#
+#dir_resolve()
+#{
+#cd "$1" 2>/dev/null || return $?  # cd to desired directory; if fail, quell any error messages but return exit status
+#echo "`pwd -P`" # output full, link-resolved path
+#}
+#
+#abs_path="`dir_resolve \"/Users/runner/Library/Developer/Xcode/DerivedData/E2EApp-*/DerivedData/Build/Products/Debug-iphoneos\"`"
 
-pwd
+#appcenter test run xcuitest \
+#--app "ashley.arthur-vonage.com/IOS-SDK_TEST" \
+#--devices "ashley.arthur-vonage.com/ios11" \
+#--test-series "master" \
+#--locale "en_US" \
+#--token "1b2050ed79bfa481249056ef0970e19938771312" \
+#--build-dir "$abs_path"
 
-ls -lah
+echo $APPCENTER_OUTPUT_DIRECTORY
 
-dir_resolve()
-{
-cd "$1" 2>/dev/null || return $?  # cd to desired directory; if fail, quell any error messages but return exit status
-echo "`pwd -P`" # output full, link-resolved path
-}
-
-abs_path="`dir_resolve \"/Users/runner/Library/Developer/Xcode/DerivedData/E2EApp-*/DerivedData/Build/Products/Debug-iphoneos\"`"
-
-appcenter test run xcuitest \
---app "ashley.arthur-vonage.com/IOS-SDK_TEST" \
---devices "ashley.arthur-vonage.com/ios11" \
---test-series "master" \
---locale "en_US" \
---token "1b2050ed79bfa481249056ef0970e19938771312" \
---build-dir "$abs_path"
+ls -lah $APPCENTER_OUTPUT_DIRECTORY
