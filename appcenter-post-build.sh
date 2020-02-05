9d88e1d8d6654dcdc784543c200eaa4e37efef20
Sergei Rastrigin <sergei.rastrigin@vonage.com>
2020-02-05 15:06:20 +0000

    revert changes

--- appcenter-post-build.sh
+++ appcenter-post-build.sh
@@ -1,9 +1,9 @@
-#!/usr/bin/env bash
-
-appcenter test run xcuitest \
---app "ashley.arthur-vonage.com/IOS-SDK_TEST" \
---devices "ashley.arthur-vonage.com/ios11" \
---test-series "master" \
---locale "en_US" \
---token "1b2050ed79bfa481249056ef0970e19938771312" \
---build-dir "DerivedData/Build/Products/Debug-iphoneos"
+##!/usr/bin/env bash
+#
+#appcenter test run xcuitest \
+#--app "ashley.arthur-vonage.com/IOS-SDK_TEST" \
+#--devices "ashley.arthur-vonage.com/ios11" \
+#--test-series "master" \
+#--locale "en_US" \
+#--token "1b2050ed79bfa481249056ef0970e19938771312" \
+#--build-dir "DerivedData/Build/Products/Debug-iphoneos"
