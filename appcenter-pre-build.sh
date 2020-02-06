8a7bb4dcc5173483afbf9e1fc444ba3a0f85a5bf
Sergei Rastrigin <sergei.rastrigin@vonage.com>
2020-02-06 15:36:28 +0000

    add github token to pre-build script

--- appcenter-pre-build.sh
+++ appcenter-pre-build.sh
@@ -2,4 +2,4 @@
 
 echo "HERE ⭐️"
 
-pod repo add NEXMO_PRIVATE_COCOAPODS git@github.com:Vonage/PrivateCocoapodsSpecs.git
+pod repo add NEXMO_PRIVATE_COCOAPODS https://RastriginSergey:fc827ef69f51ebd2e0af541e5fc1960d601aab15@github.com/Vonage/PrivateCocoapodsSpecs
