d017cb50a45e1f06619087357839830cce2cc90f
Sergei Rastrigin <sergei.rastrigin@vonage.com>
2020-02-06 14:49:46 +0000

    appcenter pre build file readded

--- appcenter-pre-build.sh
+++ appcenter-pre-build.sh
@@ -2,4 +2,4 @@
 
 echo "pre build script was executed"
 
-pod repo add NEXMO_PRIVATE_COCOAPODS git@github.com:Vonage/PrivateCocoapodsSpecs.git
+#pod repo add NEXMO_PRIVATE_COCOAPODS git@github.com:Vonage/PrivateCocoapodsSpecs.git
