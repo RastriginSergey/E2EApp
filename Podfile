cdd15e4a55e48031e80de0071a62fee25f46f24e
Sergei Rastrigin <sergei.rastrigin@vonage.com>
2020-02-07 15:47:25 +0000

    interpolate a github access token from an environment variable

--- Podfile
+++ Podfile
@@ -1,6 +1,6 @@
 platform :ios, '10.2'
 
-source 'https://RastriginSergey:2fe48fa03363239c7995afff7dc5e1660aa6423d@github.com/Vonage/PrivateCocoapodsSpecs'
+source "https://RastriginSergey:#{ENV['token']}@github.com/Vonage/PrivateCocoapodsSpecs"
 
 target 'E2EApp' do
   pod 'NexmoClient', '2.1.0'
--- appcenter-pre-build.sh
+++ appcenter-pre-build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 echo "HERE ⭐️"
+pod repo add NEXMO_PRIVATE_COCOAPODS https://RastriginSergey:$token@github.com/Vonage/PrivateCocoapodsSpecs
 
-pod repo add NEXMO_PRIVATE_COCOAPODS https://RastriginSergey:2fe48fa03363239c7995afff7dc5e1660aa6423d@github.com/Vonage/PrivateCocoapodsSpecs
