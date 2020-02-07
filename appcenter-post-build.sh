ddb021ef10789263de41188e208314765d9dd749
Sergei Rastrigin <sergei.rastrigin@vonage.com>
2020-02-07 12:46:49 +0000

    try on devices with version 13.2

--- appcenter-post-build.sh
+++ appcenter-post-build.sh
@@ -6,4 +6,9 @@ xcrun xcodebuild build-for-testing \
   -scheme $APPCENTER_XCODE_SCHEME \
   -derivedDataPath DerivedData
 
-appcenter test run xcuitest --app "ashley.arthur-vonage.com/IOS-SDK_TEST" --devices "ashley.arthur-vonage.com/tier-2" --test-series "set-of-devices" --locale "en_US" --build-dir "DerivedData/Build/Products/Debug-iphoneos" --token "1b2050ed79bfa481249056ef0970e19938771312"
+
+# set of devices (versions from 10.2 to 13.2)
+#appcenter test run xcuitest --app "ashley.arthur-vonage.com/IOS-SDK_TEST" --devices "ashley.arthur-vonage.com/tier-2" --test-series "set-of-devices" --locale "en_US" --build-dir "DerivedData/Build/Products/Debug-iphoneos" --token "1b2050ed79bfa481249056ef0970e19938771312"
+
+# ios 13.2 only
+appcenter test run xcuitest --app "ashley.arthur-vonage.com/IOS-SDK_TEST" --devices "ashley.arthur-vonage.com/ios11" --test-series "master" --locale "en_US" --build-dir "DerivedData/Build/Products/Debug-iphoneos" --token "1b2050ed79bfa481249056ef0970e19938771312"
