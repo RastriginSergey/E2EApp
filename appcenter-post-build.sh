aedab008557266558668c7debc98dd8aaf320c59
Sergei Rastrigin <sergei.rastrigin@vonage.com>
2020-02-07 11:47:50 +0000

    run on tier 2 device set

--- appcenter-post-build.sh
+++ appcenter-post-build.sh
@@ -6,11 +6,4 @@ xcrun xcodebuild build-for-testing \
   -scheme $APPCENTER_XCODE_SCHEME \
   -derivedDataPath DerivedData
 
-
-appcenter test run xcuitest \
---app "ashley.arthur-vonage.com/IOS-SDK_TEST" \
---devices c56c42b8 \
---test-series "set-of-devices" \
---locale "en_US" \
---token "1b2050ed79bfa481249056ef0970e19938771312" \
---build-dir "DerivedData/Build/Products/Debug-iphoneos"
+appcenter test run xcuitest --app "ashley.arthur-vonage.com/IOS-SDK_TEST" --devices "ashley.arthur-vonage.com/tier-2" --test-series "set-of-devices" --locale "en_US" --build-dir "DerivedData/Build/Products/Debug-iphoneos" --token "1b2050ed79bfa481249056ef0970e19938771312"
