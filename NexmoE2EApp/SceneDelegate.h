b4e09d7835b8ded6c7ae306ed8472be41a7141f7
Sergei Rastrigin <sergei.rastrigin@vonage.com>
2020-02-10 12:48:44 +0000

    add targets for appcenter.ms

---
+++ NexmoE2EApp/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  NexmoE2EApp
+//
+//  Created by Sergei Rastrigin on 10/02/2020.
+//  Copyright © 2020 Vonage. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
---
+++ NexmoE2EApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NexmoE2EApp
+//
+//  Created by Sergei Rastrigin on 10/02/2020.
+//  Copyright © 2020 Vonage. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
