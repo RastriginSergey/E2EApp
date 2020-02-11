18608664d5cd2ddce50b69b0f7df7d3c5306b558
ashley.arthur <ashley.arthur@vonage.com>
2020-02-11 16:25:18 +0000

    debug print

--- appcenter-post-build.sh
+++ appcenter-post-build.sh
@@ -1,5 +1,8 @@
 echo "Executing post build script"
 
+bash --version
+which getopt
+
 appcenter test run xcuitest --app "My_Test/AppForE2E" \
 --devices "My_Test/iphone-7-plus-13-dot-3-1" \
 --test-series "master" \
