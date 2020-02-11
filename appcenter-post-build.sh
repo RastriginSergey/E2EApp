6196ef6c5d45548948554a673019c22b393660ae
ashley.arthur <ashley.arthur@vonage.com>
2020-02-11 16:44:19 +0000

    install brew

--- appcenter-post-build.sh
+++ appcenter-post-build.sh
@@ -3,7 +3,9 @@ echo "Executing post build script"
 bash --version
 which getopt
 
+CI=1 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew install gnu-getopt
+brew link --force gnu-getopt
 
 appcenter test run xcuitest --app "My_Test/AppForE2E" \
 --devices "My_Test/iphone-7-plus-13-dot-3-1" \
