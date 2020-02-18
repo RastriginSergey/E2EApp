ecc2d586a62fb5a578beb30333a8c7a88743872e
Sergei Rastrigin <sergei.rastrigin@vonage.com>
2020-02-18 10:39:59 +0000

    add awscli

--- appcenter-post-build.sh
+++ appcenter-post-build.sh
@@ -1,27 +1,29 @@
-rm -rf DerivedData
-xcrun xcodebuild build-for-testing \
-  -configuration Debug \
-  -workspace $APPCENTER_XCODE_PROJECT \
-  -sdk iphoneos \
-  -scheme $APPCENTER_XCODE_SCHEME \
-  -derivedDataPath DerivedData
+aws --version
 
-cd ./DerivedData/Build/Products
-
-zip -rX Products.zip *
-
-zipinfo Products.zip
-
-
-# version of devices 10.2
-#appcenter test run xcuitest \
-#--app "My_Test/AppForE2E" \
-#--devices "My_Test/10-dot-2-1" \
-#--test-series "launch-tests" \
-#--locale "en_US" \
-#--build-dir "DerivedData/Build/Products/Debug-iphoneos" \
-#--token "1b2050ed79bfa481249056ef0970e19938771312"
-
-
-# versions 13, 12, 11 and 10
-#appcenter test run xcuitest --app "My_Test/AppForE2E" --devices "My_Test/5-devices" --test-series "launch-tests" --locale "en_US" --build-dir "DerivedData/Build/Products/Debug-iphoneos" --token "1b2050ed79bfa481249056ef0970e19938771312"
+#rm -rf DerivedData
+##xcrun xcodebuild build-for-testing \
+##  -configuration Debug \
+##  -workspace $APPCENTER_XCODE_PROJECT \
+##  -sdk iphoneos \
+##  -scheme $APPCENTER_XCODE_SCHEME \
+##  -derivedDataPath DerivedData
+##
+##cd ./DerivedData/Build/Products
+##
+##zip -rX Products.zip *
+##
+##zipinfo Products.zip
+#
+#
+## version of devices 10.2
+##appcenter test run xcuitest \
+##--app "My_Test/AppForE2E" \
+##--devices "My_Test/10-dot-2-1" \
+##--test-series "launch-tests" \
+##--locale "en_US" \
+##--build-dir "DerivedData/Build/Products/Debug-iphoneos" \
+##--token "1b2050ed79bfa481249056ef0970e19938771312"
+#
+#
+## versions 13, 12, 11 and 10
+##appcenter test run xcuitest --app "My_Test/AppForE2E" --devices "My_Test/5-devices" --test-series "launch-tests" --locale "en_US" --build-dir "DerivedData/Build/Products/Debug-iphoneos" --token "1b2050ed79bfa481249056ef0970e19938771312"
--- appcenter-pre-build.sh
+++ appcenter-pre-build.sh
@@ -3,3 +3,4 @@
 echo "HERE ⭐️"
 pod repo add NEXMO_PRIVATE_COCOAPODS https://RastriginSergey:$token@github.com/Vonage/PrivateCocoapodsSpecs
 
+pip install awscli
