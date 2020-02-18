d74ca7e6ba3845618d3db72cab8a0f303c074f67
Sergei Rastrigin <sergei.rastrigin@vonage.com>
2020-02-18 18:02:03 +0000

    print env

--- appcenter-pre-build.sh
+++ appcenter-pre-build.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
 echo "HERE ⭐️"
-pod repo add NEXMO_PRIVATE_COCOAPODS https://RastriginSergey:$token@github.com/Vonage/PrivateCocoapodsSpecs
+pod repo add NEXMO_PRIVATE_COCOAPODS https://RastriginSergey:$GITHUB_TOKEN@github.com/Vonage/PrivateCocoapodsSpecs
+
+env
 
 pip install awscli
