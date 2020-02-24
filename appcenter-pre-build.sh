eb35eb08d8a8fb18a5db04a5155c230c473698dd
Sergei Rastrigin <sergei.rastrigin@vonage.com>
2020-02-24 11:06:39 +0000

    download .env file from s3 and load it into memory

--- appcenter-pre-build.sh
+++ appcenter-pre-build.sh
@@ -4,3 +4,9 @@ echo "Executing post build script"
 pod repo add NEXMO_PRIVATE_COCOAPODS https://RastriginSergey:$GITHUB_TOKEN@github.com/Vonage/PrivateCocoapodsSpecs
 
 pip install awscli
+
+aws s3 cp s3://nexmo-sdk-ci/branches/${APPCENTER_BRANCH}/sdk_versions/${CLIENT_VERSION}.env ./config.env
+
+source ./config.env
+
+printenv | grep "GITHUB_TOKEN"
