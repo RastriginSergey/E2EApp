081243a831001dfd9ddeb434ce2895c4c0c641ba
Sergei Rastrigin <sergei.rastrigin@vonage.com>
2020-02-24 17:24:13 +0000

    fix typo

--- appcenter-post-clone.sh
+++ appcenter-post-clone.sh
@@ -2,7 +2,7 @@ echo "Executing post clone script"
 
 pip install awscli
 
-aws s3 cp s3://nexmo-sdk-ci/branches/${APPCENTER_BRANCH}/built-id/${APPCENTER_BUILD_ID}.env ./config.env
+aws s3 cp s3://nexmo-sdk-ci/branches/${APPCENTER_BRANCH}/build-id/${APPCENTER_BUILD_ID}.env ./config.env
 
 cat ./config.env
 
