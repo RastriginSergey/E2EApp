726311f5f475264dd1970d42c723c28cca8baf6d
Sergei Rastrigin <sergei.rastrigin@vonage.com>
2020-02-24 17:14:12 +0000

    run config file

---
+++ appcenter-post-clone.sh
@@ -0,0 +1,11 @@
+echo "Executing post clone script"
+
+pip install awscli
+
+aws s3 cp s3://nexmo-sdk-ci/branches/${APPCENTER_BRANCH}/built-id/${APPCENTER_BUILD_ID}.env ./config.env
+
+cat ./config.env
+
+source ./config.env
+
+printenv | grep "GITHUB_TOKEN"
--- appcenter-pre-build.sh
+++ appcenter-pre-build.sh
@@ -2,11 +2,3 @@ echo "Executing pre build script"
 
 # Add Private CocoaPods
 pod repo add NEXMO_PRIVATE_COCOAPODS https://RastriginSergey:$GITHUB_TOKEN@github.com/Vonage/PrivateCocoapodsSpecs
-
-pip install awscli
-
-aws s3 cp s3://nexmo-sdk-ci/branches/${APPCENTER_BRANCH}/sdk_versions/${CLIENT_VERSION}.env ./config.env
-
-source ./config.env
-
-printenv | grep "GITHUB_TOKEN"
