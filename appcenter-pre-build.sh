03718559ec49ef2ef9390226d81593462318bede
Sergei Rastrigin <sergei.rastrigin@vonage.com>
2020-02-24 11:28:37 +0000

    fix typo

--- appcenter-pre-build.sh
+++ appcenter-pre-build.sh
@@ -1,4 +1,4 @@
-echo "Executing post build script"
+echo "Executing pre build script"
 
 # Add Private CocoaPods
 pod repo add NEXMO_PRIVATE_COCOAPODS https://RastriginSergey:$GITHUB_TOKEN@github.com/Vonage/PrivateCocoapodsSpecs
