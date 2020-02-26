cf0a3a622e1487e16fb00648221fb05ee5405fbb
Nicola Di Pol <nicola.dipol@vonage.com>
2020-02-26 16:20:34 +0000

    Added config.env sourcing to pre-build script

--- appcenter-pre-build.sh
+++ appcenter-pre-build.sh
@@ -1,4 +1,6 @@
 echo "Executing pre build script"
 
+source ./config.env
+
 # Add Private CocoaPods
 pod repo add NEXMO_PRIVATE_COCOAPODS https://RastriginSergey:$GITHUB_TOKEN@github.com/Vonage/PrivateCocoapodsSpecs
