c3d20759a35ba457f00d8eaf76037ef06cf47c3f
Nicola Di Pol <nicola.dipol@vonage.com>
2020-02-26 15:34:57 +0000

    Removed git-ignored files

--- Pods/Target Support Files/Pods-E2EApp-E2EAppUITests/Pods-E2EApp-E2EAppUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_E2EApp_E2EAppUITests : NSObject
-@end
-@implementation PodsDummy_Pods_E2EApp_E2EAppUITests
-@end
--- Pods/Target Support Files/Pods-E2EApp/Pods-E2EApp-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_E2EApp : NSObject
-@end
-@implementation PodsDummy_Pods_E2EApp
-@end
